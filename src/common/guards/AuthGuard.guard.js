bfd33eda538f413892f10b49b7bad832549772da
erfan8005 <erfinakhoda@gmail.com>
2024-11-26 21:16:49 +0330

    a bug fixed at AuthGuard

--- src/common/guards/AuthGuard.guard.js
+++ src/common/guards/AuthGuard.guard.js
@@ -6,8 +6,9 @@ async function AuthLoginGuard(req,res,next) {
     const {access_Token} = req.cookies;
     if(!access_Token) return next();
     const data = VerifyToken(access_Token);
-    const user =await userModel.findById(data.id);
+    const user =await userModel.findById(data.id).lean();
     if(!user) return next();
+    req.user = user;
     return res.status(200).send({
         message : AuthMessages.alreadyLoggedIn,
         statusCode : res.statusCode
